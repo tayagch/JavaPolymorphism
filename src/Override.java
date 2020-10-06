c92f6010f7955af08af726bba623922c989c63c0
CHRISTIAN MICHAEL TAYAG (PLAN-IRIS-ISD-OOCLL/MNL) <TAYAGCH@OOCL.COM>
2020-10-06 12:46:10 +0800

    Resubmit Answers

---
+++ src/Override.java
@@ -0,0 +1,6 @@
+public class Override extends Printer{
+    @java.lang.Override
+    public void print() {
+        System.out.println("This will override print method in Printer class");
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,25 @@
-public class Test {
+public class Test extends Printer{
     public static void main(String[] args){
+        System.out.println("For number 1 requirement.......");
+        Override override = new Override();
+        override.print();
+        testOverload("Christian i am not overload");
+        testOverload("Christian", "i am overload");
+
+        System.out.println("For number 2 requirement.......");
         Printer printer = new Printer();
         printer.print();
         printer = new ColorPrinter();
         printer.print();
         printer = new BlacknWhitePrint();
         printer.print();
+    }
+
+    public static void testOverload(String name){
+        System.out.println(name);
+    }
+
+    public static void testOverload(String name, String overload){
+        System.out.println(name + " " + overload);
     }
 }
